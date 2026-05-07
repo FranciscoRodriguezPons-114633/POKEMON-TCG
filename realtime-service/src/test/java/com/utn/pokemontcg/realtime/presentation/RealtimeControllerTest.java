04c7220176c39078b5c2cfefab47ec0327646215
FranciscoRodriguezPons-114633 <143284270+FranciscoRodriguezPons-114633@users.noreply.github.com>
2026-05-07 01:36:58 -0300

    Replace deprecated @MockBean with @MockitoBean in web slice tests

--- realtime-service/src/test/java/com/utn/pokemontcg/realtime/presentation/RealtimeControllerTest.java
+++ realtime-service/src/test/java/com/utn/pokemontcg/realtime/presentation/RealtimeControllerTest.java
@@ -6,7 +6,7 @@ import com.utn.pokemontcg.realtime.dto.GameEventMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,7 +29,7 @@ class RealtimeControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private EventBroadcastService eventBroadcastService;
 
     @Test
